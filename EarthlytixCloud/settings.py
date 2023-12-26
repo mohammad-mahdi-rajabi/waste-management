0887ade7263bc885711f8d90ef206a0921a23db0
mh.heydari <mohammadhsein9@gmail.com>
2023-12-26 11:01:37 +0330

    container

--- EarthlytixCloud/settings.py
+++ EarthlytixCloud/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
             'NAME': "form",
             'USER': "postgres",
             'PASSWORD': "1234",
-            'HOST': "localhost",
+            'HOST': "postgres",
             'PORT': "5432",
         }
     }
